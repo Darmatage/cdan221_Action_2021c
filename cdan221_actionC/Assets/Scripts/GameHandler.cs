e7f8b854b1a435b17911c48a10fa61c01f659920
DariusSerebrova <79662013+DariusSerebrova@users.noreply.github.com>
2021-04-08 21:15:39 -0400

    began spawn, respawn & gamehandler scripts (not done)

---
+++ cdan221_actionC/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameHandler : MonoBehaviour
+{
+
+    private GameObject player;
+      public static int playerHealth;
+    public int StartPlayerHealth = 100;
+    public GameObject healthText;
+
+    public static int gotTokens = 0;
+    public bool isDefending = false;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        playerHealth = StartPlayerHealth;
+        updateHealthDisplay();
+    }
+
+    public void playerGetHit(int damage)
+    {
+        if (isDefending == false)
+        {
+            playerHealth -= damage;
+            updateHealthDisplay();
+            player.GetComponent<PlayerHurt>().playerHit();
+        }
+
+        if (playerHealth >= 100)
+        {
+            playerHealth = 100;
+        }
+
+        if (playerHealth <= 0)
+        {
+            playerHealth = 0;
+            playerDies();
+        }
+    }
+
+    public void updateHealthDisplay()
+    {
+        //Text healthTextTemp = healthText.GetComponent<Text>();
+        //healthTextTemp.text = "PLAYER HEALTH: " + playerHealth;
+    }
+
+
+    public void playerDies()
+    {
+        player.GetComponent<PlayerHurt>().playerDead();
+        StartCoroutine(DeathPause());
+    }
+
+    IEnumerator DeathPause()
+    {
+        player.GetComponent<PlayerMove>().isAlive = false;
+        player.GetComponent<PlayerJump>().isAlive = false;
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene("LoseScreen");
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("StartScreen");
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+                Application.Quit();
+#endif
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+}
---
+++ cdan221_actionC/Assets/Scripts/PlayerBottomRespawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBottomRespawn : MonoBehaviour
+{
+
+    public GameHandler gameHandler;
+    public Transform playerPos;
+    public Transform pSpawn;
+    //public int damage = 10;
+
+    void Start()
+    {
+        playerPos = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        gameHandler = GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>();
+    }
+
+    void Update()
+    {
+        if (playerPos != null)
+        {
+            if (transform.position.y >= playerPos.position.y)
+            {
+                //instantiate a particle effect
+                Debug.Log("I am going back to the start");
+                //gameHandler.TakeDamage(damage);
+                //Vector3 pSpn2 = new Vector3(pSpawn.position.x, pSpawn.position.y, pPos.position.z);
+                //playerPos.position = pSpn2;
+            }
+        }
+    }
+}
