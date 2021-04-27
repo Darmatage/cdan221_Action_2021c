358c03666812eb3a9aa422f4cea55b3667a5f352
DariusSerebrova <79662013+DariusSerebrova@users.noreply.github.com>
2021-04-27 18:39:07 -0400

    Added LoadNextLevel script

---
+++ cdan221_actionC/Assets/Scripts/LoadNextLevel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextLevel : MonoBehaviour
+
+{
+    public string NextScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D coll)
+    {
+
+        if (coll.gameObject.tag == "Player")
+        {
+
+            //Here it would use the "level" variable to load the next scene
+            SceneManager.LoadScene(NextScene);
+            //Debug.Log("I hit the collider");
+        }
+    }
+}
+
