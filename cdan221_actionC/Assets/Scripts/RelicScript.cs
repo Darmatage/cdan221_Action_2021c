8a4a5d5c392270c41b2795af740e78dd3a119616
DariusSerebrova <79662013+DariusSerebrova@users.noreply.github.com>
2021-05-03 21:21:37 -0400

    created tutorial relic

---
+++ cdan221_actionC/Assets/Scripts/RelicScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RelicScript : MonoBehaviour
+{
+    private GameObject player;
+    public Player_Soulsight playerSoulSight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        playerSoulSight = GameObject.FindWithTag("Player").GetComponent<Player_Soulsight>();
+        player.GetComponent<Player_Soulsight>().enabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+
+            player.GetComponent<Player_Soulsight>().enabled = true;
+            Destroy(gameObject);
+        }
+    }
+}
