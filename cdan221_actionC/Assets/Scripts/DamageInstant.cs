38d524fa2bb5f7063a661aea77b8f1dc063bc658
DariusSerebrova <79662013+DariusSerebrova@users.noreply.github.com>
2021-05-05 16:46:09 -0400

    fixed lava level 7

--- cdan221_actionC/Assets/Scripts/DamageInstant.cs
+++ cdan221_actionC/Assets/Scripts/DamageInstant.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class DamageInstant : MonoBehaviour {
 
        public GameHandler gameHandlerObj;
-       public int damage = 10;
+       public int damage = 100;
        public Transform backToStart; 
 	   //uncomment this line for "auto-death," to zap the Player back to start
 
@@ -17,8 +17,8 @@ public class DamageInstant : MonoBehaviour {
 
        public void OnCollisionEnter2D(Collision2D other) {
               if (other.gameObject.tag == "Player") {
-                     gameHandlerObj.playerGetHit(damage);
-                     //other.transform.position = new Vector3(backToStart.position.x, backToStart.position.y, backToStart.position.z);
+                     //gameHandlerObj.playerGetHit(damage);
+                     other.transform.position = new Vector3(backToStart.position.x, backToStart.position.y, backToStart.position.z);
               }
        }
 
