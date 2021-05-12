2617eda60a5adad268ca0507180fa48ff0245824
mackenzitgreen <mackenzitgreen@gmail.com>
2021-05-11 20:45:37 -0400

    Finished SFX Volume Slider

--- cdan221_actionC/Assets/Scripts/PauseMenu.cs
+++ cdan221_actionC/Assets/Scripts/PauseMenu.cs
@@ -10,8 +10,11 @@ public class PauseMenu : MonoBehaviour {
       public static bool GameisPaused = false;
       public GameObject pauseMenuUI;
       public AudioMixer mixer;
+	  public AudioMixer sfxmixer;
       public static float volumeLevel = 1.0f;
+	  public static float volumeLevel2 = 1.0f;
       private Slider sliderVolumeCtrl;
+	  private Slider sliderVolumeCtrl2;
 
       void Awake (){
             SetLevel (volumeLevel);
@@ -20,6 +23,14 @@ public class PauseMenu : MonoBehaviour {
                   sliderVolumeCtrl = sliderTemp.GetComponent<Slider>();
                   sliderVolumeCtrl.value = volumeLevel;
             }
+			
+			SetLevelSFX (volumeLevel2);
+            GameObject sliderTemp2 = GameObject.FindWithTag("PauseMenuSlider2");
+            if (sliderTemp2 != null){
+                  sliderVolumeCtrl2 = sliderTemp2.GetComponent<Slider>();
+                  sliderVolumeCtrl2.value = volumeLevel2;
+            }
+			
       }
 
       void Start (){
@@ -67,6 +78,11 @@ public class PauseMenu : MonoBehaviour {
       public void SetLevel (float sliderValue){
             mixer.SetFloat("MusicVolume", Mathf.Log10 (sliderValue) * 20);
             volumeLevel = sliderValue;
+      }
+	  
+	  public void SetLevelSFX (float sliderValue){
+            sfxmixer.SetFloat("SFXVolume", Mathf.Log10 (sliderValue) * 20);
+            volumeLevel2 = sliderValue;
       }
 
 }
